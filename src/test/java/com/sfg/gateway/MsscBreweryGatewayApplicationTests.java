74e26e1933e33a556ae9862e6ac5a03f6d8afd8e
John Thompson <john@springframework.guru>
2020-03-04 20:54:24 -0500

    initial add of project

---
+++ src/test/java/com/sfg/gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

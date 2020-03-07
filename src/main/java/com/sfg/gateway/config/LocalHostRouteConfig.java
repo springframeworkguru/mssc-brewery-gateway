bce798289032e15baa21a3ed7adf8bee4ef8398d
John Thompson <john@springframework.guru>
2020-03-07 11:41:07 -0500

    adding route for inventory service. closes #4

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -20,6 +20,9 @@ public class LocalHostRouteConfig {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("http://localhost:8081")
                         .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082")
+                        .id("inventory-service"))
                 .build();
     }
 }

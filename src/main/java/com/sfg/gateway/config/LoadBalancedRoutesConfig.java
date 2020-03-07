76ccec0b5469890989afae71df09bf74e044ccaf
John Thompson <john@springframework.guru>
2020-03-07 16:38:11 -0500

    adding Eureka LB endpoints. closes #6

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -9,22 +9,23 @@ import org.springframework.context.annotation.Profile;
 /**
  * Created by jt on 3/7/20.
  */
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRoutesConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080")
+                        .uri("lb://beer-service")
                         .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers/**")
-                        .uri("http://localhost:8081")
+                        .uri("lb://order-service")
                         .id("order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082")
+                        .uri("lb://inventory-service")
                         .id("inventory-service"))
                 .build();
     }
+
 }

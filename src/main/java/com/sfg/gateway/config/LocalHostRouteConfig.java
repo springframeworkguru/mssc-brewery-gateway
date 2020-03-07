9bcffb88bcc7adb78d32a40947c9b178d56b9225
John Thompson <john@springframework.guru>
2020-03-07 11:26:11 -0500

    adding route for orders closes #3

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -17,6 +17,9 @@ public class LocalHostRouteConfig {
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
                 .build();
     }
 }

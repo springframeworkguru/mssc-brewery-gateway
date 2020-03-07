34c8a430e201a720e442d3dbbbfbb4798f7ccbf7
John Thompson <john@springframework.guru>
2020-03-07 09:34:47 -0500

    adding localhost config for beer service - closes #2

---
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,22 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jt on 3/7/20.
+ */
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .build();
+    }
+}
